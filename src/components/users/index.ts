a0af7bef2677f262270216298d1c112178f2a07d
Fernando Parra <fernandoxe@gmail.com>
2024-07-08 12:45:00 +0200

    chore: users list

---
+++ src/components/users/index.ts
@@ -0,0 +1,34 @@
+import { LitElement, css, html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { repeat } from 'lit/directives/repeat.js';
+import { UserI } from '../../interfaces';
+
+@customElement('pl-users')
+export class Users extends LitElement {
+  static styles = css`
+    :host {
+      font-size: 0.75rem;
+      line-height: 1;
+      user-select: none;
+    }
+    .list {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+      padding: 0.5rem 1rem;
+    }
+  `;
+
+  @property({ type: Array })
+  users: UserI[] = [];
+
+  render() {
+    return html`
+      ${repeat(this.users, user => user.id, user => html`
+        <div class="list">
+          <div>${user.name}</div>
+        </div>
+      `)}
+    `;
+  }
+}
